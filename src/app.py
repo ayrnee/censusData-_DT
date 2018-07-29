c2a63ae56915249dd3df6b581a339c4ab6dee57a
ayrnee <ayrnee@gmail.com>
2018-07-29 11:36:14 -0400

    bug fix

--- src/app.py
+++ src/app.py
@@ -6,31 +6,31 @@ import numpy as np
 from collections import Counter
 from sklearn.model_selection import train_test_split
 from sklearn import tree
+import matplotlib.pyplot as plt
+import pydot
+import pydotplus
+from sklearn.externals.six import StringIO
 
 DATA_DIR = './data'
 
 def build_feature_dict(data):
     train = data['train_x']
-
     transpose = []
     for i in range(len(train[0])):
         transpose.append(list(zip(*train)[i]))
-
     missing_feat = []
     for i in range(len(train[0])):
         categorical = Counter([s for s in transpose[i] if not s.isdigit() and s.strip() != '?'])
         continuous = np.mean([int(x) for x in transpose[i] if x.isdigit()])
-
         if len(categorical.keys()) > 0:
             missing_feat.append(categorical.most_common(1)[0][0])
         else:
             missing_feat.append(continuous)
-
     for sample in train:
         for i in range(len(sample)):
             if sample[i].strip() == '?':
                 sample[i] = missing_feat[i]
-
+    return
 
 def expand_sample(elements, new_sample, target):
     for element in elements:
@@ -38,6 +38,7 @@ def expand_sample(elements, new_sample, target):
             new_sample.append(1)
         else:
             new_sample.append(0)
+    return
 
 def make_numeric(data, f_name = 'features.txt', x = 'train_x', y = 'train_y'):
     features = {}
@@ -60,24 +61,22 @@ def make_numeric(data, f_name = 'features.txt', x = 'train_x', y = 'train_y'):
         new_samples.append(new_sample)
 
     data[x] = new_samples
-
     data[y] = map(lambda x: 1 if x == '<=50K' else -1, data[y])
+    return
 
 def read_file(f_name):
     data = []
     with open('./data/' + f_name) as file:
         for line in file:
             data.append(line.strip().split(','))
-
     return data
 
 def feature_transform(raw_data):
     data_x = []
     data_y = []
-
     for i in range(len(raw_data)):
         data_x.append(raw_data[i][:12])
-        data_y.append(raw_data[i][12:][0].strip())
+        data_y.append(raw_data[i][12:][0].strip(' .\t\n'))
 
     return data_x, data_y
 
@@ -88,40 +87,63 @@ def sample_split(data):
     split_data = train_test_split(data_x, data_y, train_size = 0.3)
     data['train_x'], data['train_y'] = split_data[0], split_data[2]
     data['val_x'], data['val_y'] = split_data[1], split_data[3]
-
-def DT_Train(data, m_depth, min_samples):
-    train_x, train_y = data['train_x'], data['train_y']
-
-    clf = tree.DecisionTreeClassifier(max_depth = m_depth, min_samples_leaf = min_samples)
-    clf.fit(train_x, train_y)
-    return clf
+    return
 
 def DT_Test(data, clf, x, y):
     test_x = data[x]
     test_y = data[y]
-
     return clf.score(test_x, test_y)
 
 def optimize_params(data):
+    train_x = data['train_x']
+    train_y = data['train_y']
     max_depth = range(1,31)
     min_samples_leaf = range(1,51)
 
-    results = []
+    acc_opt = -1
+    max_depth_opt = 0
+    min_samples_leaf_opt = 0
 
-    max_acc = -1
-    args_max = []
+    graph_vals = []
     for i in max_depth:
-        for j in min_samples_leaf:
-            clf = DT_Train(data, i, j)
-            train_acc = DT_Test(data, clf, 'train_x', 'train_y')
-            val_acc = DT_Test(data, clf, 'val_x', 'val_y')
-            max_acc, args_max = (val_acc, [i,j]) if max_acc < val_acc else (max_acc, args_max)
-            print max_acc, args_max
-            results.append([i, j, train_acc, val_acc])
-    print max_acc, args_max
-    return args_max
+        clf = tree.DecisionTreeClassifier(max_depth = i).fit(train_x, train_y)
+        train_acc = DT_Test(data, clf, 'train_x', 'train_y')
+        val_acc = DT_Test(data, clf, 'val_x', 'val_y')
+        acc_opt, max_depth_opt = (val_acc, i) if acc_opt < val_acc else (acc_opt, max_depth_opt)
+        graph_vals.append([i, train_acc, val_acc])
+
+    plt.scatter(
+        [x[0] for x in graph_vals],
+        [x[1] for x in graph_vals]
+    )
+    plt.scatter(
+        [x[0] for x in graph_vals],
+        [x[2] for x in graph_vals]
+    )
+    plt.savefig('depth_plot')
+    print acc_opt, max_depth_opt
+    acc_opt = -1
+    plt.clf()
+    graph_vals = []
+    for j in min_samples_leaf:
+        clf = tree.DecisionTreeClassifier(min_samples_leaf = j).fit(train_x, train_y)
+        train_acc = DT_Test(data, clf, 'train_x', 'train_y')
+        val_acc = DT_Test(data, clf, 'val_x', 'val_y')
+        acc_opt, min_samples_leaf_opt = (val_acc, j) if acc_opt < val_acc else (acc_opt, min_samples_leaf_opt)
+        graph_vals.append([j, train_acc, val_acc])
 
 
+    plt.scatter(
+        [x[0] for x in graph_vals],
+        [x[1] for x in graph_vals]
+    )
+    plt.scatter(
+        [x[0] for x in graph_vals],
+        [x[2] for x in graph_vals]
+    )
+    plt.savefig('leaf_plot')
+    print acc_opt, min_samples_leaf_opt
+    return
 
 def main():
     data = {}
@@ -138,15 +160,33 @@ def main():
     )
 
     build_feature_dict(data)
-    make_numeric(data)
-    make_numeric(data, 'features.txt', 'test_x', 'test_y')
+    make_numeric(
+        data=data,
+        f_name='features.txt',
+        x='train_x',
+        y='train_y'
+    )
+    make_numeric(
+        data=data,
+        f_name='features.txt',
+        x='test_x',
+        y='test_y'
+    )
     # sample_split(data)
-    #
     # optimize_params(data)
-    clf = DT_Train(data, 10, 32)
-    clf.fit(data['train_x'], data['train_y'])
-    print DT_Test(data, clf, 'train_x', 'train_y')
-    print DT_Test(data, clf, 'test_x', 'test_y')
+    train_x = data['train_x']
+    train_y = data['train_y']
+    clf = tree.DecisionTreeClassifier(max_depth = 10, min_samples_leaf = 32, criterion = 'entropy').fit(train_x, train_y)
+
+
+    dot_data = tree.export_graphviz(clf, out_file=None, max_depth=3, label="all", filled=True)
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    graph.write_pdf("tree.pdf")
+    # # clf = DT_Train(data, 10, 32)
+    # # clf.fit(data['train_x'], data['train_y'])
+    # # print DT_Test(data, clf, 'train_x', 'train_y')
+    # print DT_Test(data, clf, 'test_x', 'test_y')
+    # # print data['test_y']
 
 if __name__ == '__main__':
     main()
